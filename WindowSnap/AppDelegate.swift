322c1013c0a00c17934fd6d872247e226fa29b66
inw2st <minwestt@gmail.com>
2026-02-20 23:46:32 +0900

    Initial Commit

---
+++ WindowSnap/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  WindowSnap
+//
+//  Created by 민서 on 2/20/26.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ WindowSnap/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WindowSnap
+//
+//  Created by 민서 on 2/20/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
